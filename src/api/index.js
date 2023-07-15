013e8ce72c7a8cae1d70bf243e978628b9819092
saalu <saaluissaka@gmail.com>
2023-07-15 19:28:15 +0000

    Included chart in the dashboard

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,8 @@
 export const getOrders = () => {
-  return fetch("https://dummyjson.com/products").then((res) => res.json());
+  return fetch("https://dummyjson.com/carts/1").then((res) => res.json());
+  // .then((json) => console.log(json));
+};
+export const getRevenue = () => {
+  return fetch("https://dummyjson.com/carts").then((res) => res.json());
   // .then((json) => console.log(json));
 };
